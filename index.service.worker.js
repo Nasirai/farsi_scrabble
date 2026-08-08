5e90d23a761510b26b9c8c6116aa16fc4b7b906a
nasirai <c.nasirai@gmail.com>
2026-08-08 13:56:18 +0330

    17 mordad - III

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786180482|12792694027';
+const CACHE_VERSION = '1786184724|266711849';
 /** @type {string} */
 const CACHE_PREFIX = 'Scrabble-F-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
