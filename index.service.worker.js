75600bb864cf06e159269ad1a515e475cfa36d63
nasirai <c.nasirai@gmail.com>
2026-08-28 18:14:35 +0330

    6 shahrivar

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787765465|288963827';
+const CACHE_VERSION = '1787928211|17303777210';
 /** @type {string} */
 const CACHE_PREFIX = 'ScrabbleFa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
