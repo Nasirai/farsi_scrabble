ae2b21016715ef60f43031590602d2da1eb9b236
nasirai <c.nasirai@gmail.com>
2026-08-30 22:10:47 +0330

    final_release_05_06_08 font

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788081564|30908806';
+const CACHE_VERSION = '1788115180|1769120545';
 /** @type {string} */
 const CACHE_PREFIX = 'ScrabbleFa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
